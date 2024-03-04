fdc86286f3bfdab62072c8cdf6661b1d1d4a1bcb
Eboweme Ayo <ebowemeayo@yahoo.com>
2024-03-04 19:30:49 +0100

    updated the hero image

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,8 +1,10 @@
-
+import Hero from "../components/Hero/Hero";
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <div>
+      <Hero />
+    </div>
   )
 }
 
--- src/assets/index.js
+++ src/assets/index.js
@@ -6,5 +6,6 @@ import tag from "../assets/tag.svg";
 import question from "../assets/question.svg";
 import google from "../assets/google.svg";
 import icon from "../assets/icon.png";
+import HeroImg from "../assets/hero.png";
 
-export { logo, menu, close, answer, tag, question, google, icon }
+export { logo, menu, close, answer, tag, question, google, icon, HeroImg }
